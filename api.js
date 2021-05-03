9a40db432e2123e8e584ad5e28ebef3f5d19a8a8
Eric-Josh <techjosmar@gamail.com>
2021-05-04 00:22:19 +0100

    fix bugs

--- api.js
+++ api.js
@@ -1,4 +1,11 @@
 $(function(){
+
+	$('#logout').click(function(){
+		sessionStorage.removeItem("access_token");
+
+		var url = "file:///home/swifta/Documents/shades/dev/jquery-consume-api/index.html";
+		 $(location).attr('href',url);
+	});
 	
 	$('#login').click(function(){
 		if($('#email').val() != '' && $('#email').val() != ''){
@@ -10,9 +17,12 @@ $(function(){
 		            password: $('#pwd').val(),
 		        },
 		        success: function(result){
-		        	var url = "file:///home/swifta/Documents/shades/dev/jquery-consume-api/project.html";
+		        	$('#success').text('You have successfully login');
+		        	$('#success').css('color','green');
+		          	// console.log(result['access_token']);
+		          	sessionStorage.setItem("access_token", result['access_token']);
+		          	var url = "file:///home/swifta/Documents/shades/dev/jquery-consume-api/project.html";
 		          	$(location).attr('href',url);
-		          	console.log(result);
 		        },
 		        error: function(result){
 		        	$('#error').text('Something went wrong. Please try again!');
@@ -38,6 +48,36 @@ $(function(){
 		          	$(location).attr('href',url);
 
 		          	$('#success').text('You have successfully registered. Kindly login');
+		        	$('#success').css('color','green');
+		        },
+		        error: function(result){
+		        	$('#error').text('Something went wrong. Please try again!');
+		        	$('#error').css('color','red');
+		        }
+	    	});
+		}
+	});
+
+	$('#add').click(function(){
+		if($('#name').val() != '' && $('#desc').val() != '' && $('#cost').val() != ''){
+			$.ajax({
+		        url: "http://localhost:8000/api/projects",
+		        method: 'post',
+		        data: {
+		        	name: $('#name').val(),
+		            introduction: $('#desc').val(),
+		            location: $('#loc').val(),
+		            cost: $('#cost').val(),
+		        },
+		        headers: {
+			        "Content-Type": "application/x-www-form-urlencoded",
+			        "Authorization": "Bearer "+sessionStorage.getItem("access_token"),
+			    },
+		        success: function(result){
+		        	var url = "file:///home/swifta/Documents/shades/dev/jquery-consume-api/project.html";
+		          	$(location).attr('href',url);
+
+		          	$('#success').text('You have successfully added a new project.');
 		        	$('#success').css('color','green');
 		        },
 		        error: function(result){
